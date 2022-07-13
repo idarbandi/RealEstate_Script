d163b295acb60e31a9a75a0a018dfd9e4d3d9117
idarbandi <darbandidr99@gmail.com>
2022-07-13 20:02:45 +0430

    Added_Search_By_Costume_Filters

--- advertisements.py
+++ advertisements.py
@@ -8,19 +8,31 @@ class apartmentsell(baseclass,apartment,sell):
         self.show_description()
         self.showPrice()
 
+    def __str__(self):
+        return f"Following apartmnent is for sale with {self.price_per_meter} million"
+
 
 class apartmentrent(baseclass,apartment,rent):
     def showdetail(self):
         self.show_description()
+
+    def __str__(self):
+        return f"Following apartmnent is for rent with {self.initial_price} million"
 
 class houseSell(baseclass,house,sell):
     def show_detail(self):
         self.show_description()
         self.show_price()
 
+    def __str__(self):
+        return f"Following House is for sale with {self.price_per_meter} million"
+
 
 class houseRent(baseclass,house,rent):
     def show_detail(self):
         self.show_description()
         # self.show_price()
+
+    def __str__(self):
+        return f"Following House is for rent with {self.monthly_price} million"
 
--- main.py
+++ main.py
@@ -14,13 +14,12 @@ southy=region(name='Boston')
 myapartment = apartmentrent(has_parking=True , has_elevator=False , floor=2,
                             user=u1 , area=250 , rooms_count=3 ,built_year=2009
                             , region=Los_angeles, address=Los_angeles.name ,
-                            initial_price=200,monthly_price=20,convertable=False
+                            initial_price=600,monthly_price=20,convertable=False
                             ,discountable=True)
 
 
 
 if __name__ == "__main__":
-    print(apartmentrent.mro())
-    print(apartmentrent.manager.search(floor=2))
+    print(apartmentrent.manager.search(monthly_price__max=30))
 
 
